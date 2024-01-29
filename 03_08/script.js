a431b075f5700390c16c6dfd7a04186be5d4f69d
Arturo Salazar <arturo.v.salazar.jr@gmail.com>
2024-01-28 19:26:51 -0600

    Update 03_08 example to practice calling object methods

--- 03_08/script.js
+++ 03_08/script.js
@@ -23,3 +23,12 @@ const backpack = {
 
 console.log("The backpack object:", backpack);
 console.log("The pocketNum value:", backpack.pocketNum);
+
+console.log("Strap length L:", backpack.strapLength.left);
+console.log("Strap length R:", backpack.strapLength.right);
+
+console.log("Changing strap lengths to L:30, R:45");
+backpack.newStrapLength(30, 45);
+
+console.log("Strap length L:", backpack.strapLength.left);
+console.log("Strap length R:", backpack.strapLength.right);
