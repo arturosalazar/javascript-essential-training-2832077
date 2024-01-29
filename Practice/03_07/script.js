c488325921b4d7e8ff366138729e14d0c87abba6
Arturo Salazar <arturo.v.salazar.jr@gmail.com>
2024-01-28 19:25:15 -0600

    Complete practice activity to create a JavaScript object with properties and methods.

--- Practice/03_07/script.js
+++ Practice/03_07/script.js
@@ -7,3 +7,28 @@
  * - Find an object that has another object inside of it to create a nested object.
  * - Test your objects in the browser console by accessing the entire object and its specific properties.
  */
+
+const chest = {
+  name: "Cedar Chest",
+  material: "cedar",
+  color: "brown",
+  volume: 60,
+  box: {
+    volume: 30,
+    material: "steel",
+    empty: true,
+  },
+  lidOpen: false,
+  toggleLid: function (lidStatus) {
+    this.lidOpen = lidStatus;
+  },
+  paintOutside: function (color) {
+    this.color = color;
+    console.log("The chest is now the color", color);
+  },
+};
+
+console.log("The chest is:", chest);
+console.log("The color of the ourside of the chest is:", chest.color);
+console.log("You can use the paintOutside method to change the chest color!");
+console.log("Try it now!");
