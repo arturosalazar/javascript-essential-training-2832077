dd5a6e801a20ef4d51f10a6be2ab604ba0db8b72
Arturo Salazar <arturo.v.salazar.jr@gmail.com>
2024-01-28 19:37:11 -0600

    Update 03_09 to complete practice on creating methods

--- Practice/03_09/script.js
+++ Practice/03_09/script.js
@@ -23,4 +23,50 @@ const backpack = {
     this.strapLength.left = lengthLeft;
     this.strapLength.right = lengthRight;
   },
+  //Update Name
+  setName: function (newName) {
+    this.name = newName;
+    return this;
+  },
+  //Update Volume
+  setVolume: function (newVolume) {
+    this.volume = newVolume;
+    return this;
+  },
+  //Update Color
+  setColor: function (newColor) {
+    this.color = newColor;
+    return this;
+  },
+  //Update Number of Pockets
+  setPocketNum: function (newPocketNum) {
+    this.pocketNum = newPocketNum;
+    return this;
+  },
 };
+
+console.log("Old Values");
+console.log("Name:", backpack.name);
+console.log("Volume:", backpack.volume);
+console.log("Color:", backpack.color);
+console.log("Pocket Number:", backpack.pocketNum);
+console.log("\n");
+
+let newName = "Current Backpack";
+let newVolume = 45;
+let newColor = "blue";
+let newPocketNum = 5;
+
+console.log("Updating Values");
+backpack
+  .setName(newName)
+  .setVolume(newVolume)
+  .setColor(newColor)
+  .setPocketNum(newPocketNum);
+console.log("\n");
+
+console.log("New Values");
+console.log("Name:", backpack.name);
+console.log("Volume:", backpack.volume);
+console.log("Color:", backpack.color);
+console.log("Pocket Number:", backpack.pocketNum);
